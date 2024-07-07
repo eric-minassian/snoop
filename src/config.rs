47de3971b541b5a45e1a312c942a9a9771deec2c
Eric Minassian <minassianeric2848@gmail.com>
2024-07-07 10:49:19 -0700

    single package support & integration tests

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,34 @@
-use std::{fs::File, io::BufReader, path::PathBuf};
+use std::{
+    fs::File,
+    io::BufReader,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 use serde::Deserialize;
 
+use crate::error::{Error, Result};
+
+pub const CONFIG_FILE: &str = "snoop.json";
+
 #[derive(Deserialize, Debug)]
-pub struct Command {
+pub struct ShellCommand {
     pub name: String,
     pub command: String,
+    pub args: Option<Vec<String>>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct PackageManifest {
     #[serde(rename = "$schema")]
     _schema: Option<String>,
-    pub commands: Vec<Command>,
+    pub commands: Vec<ShellCommand>,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct PackageInfo {
     pub name: String,
-    pub root: PathBuf,
+    pub path: PathBuf,
 }
 
 #[derive(Deserialize, Debug)]
@@ -28,19 +38,49 @@ pub struct Workspace {
     pub packages: Vec<PackageInfo>,
 }
 
-pub fn load_json<T>(path: &PathBuf) -> T
-where
-    T: for<'de> Deserialize<'de>,
-{
-    let file = File::open(path).unwrap_or_else(|e| {
-        eprintln!("Failed to open file: {}", e);
-        std::process::exit(1);
-    });
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Config {
+    Workspace(Workspace),
+    Package(PackageManifest),
+}
 
+fn load_json<T: for<'de> Deserialize<'de>>(path: &PathBuf) -> Result<T> {
+    let file = File::open(path)?;
     let reader = BufReader::new(file);
+    Ok(serde_json::from_reader(reader)?)
+}
 
-    serde_json::from_reader(reader).unwrap_or_else(|e| {
-        eprintln!("Failed to parse JSON: {}", e);
-        std::process::exit(1);
-    })
+pub fn run_command(root: &Path, package_name: Option<&str>, command: &str) -> Result<()> {
+    let root_config_path = root.join(CONFIG_FILE);
+    let root_config: Config = load_json(&root_config_path)?;
+
+    let package_manifest = match root_config {
+        Config::Workspace(workspace) => {
+            let package_name = package_name.ok_or(Error::MissingPackageName)?;
+
+            let package_info = workspace
+                .packages
+                .into_iter()
+                .find(|p| p.name == package_name)
+                .ok_or_else(|| Error::PackageNotFound(package_name.to_string()))?;
+
+            let package_manifest_path = root.join(package_info.path).join(CONFIG_FILE);
+            load_json(&package_manifest_path)?
+        }
+        Config::Package(package_manifest) => package_manifest,
+    };
+
+    let shell_command = package_manifest
+        .commands
+        .into_iter()
+        .find(|c| c.name == command)
+        .ok_or_else(|| Error::CommandNotFound(command.to_string()))?;
+
+    Command::new(shell_command.command)
+        .args(shell_command.args.unwrap_or_default())
+        .status()
+        .map_err(Error::CommandExecutionFailed)?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
 mod cli;
 mod config;
+mod error;
+
+use std::process::exit;
 
 use clap::Parser;
 use cli::Args;
-use config::{load_json, PackageManifest, Workspace};
+use config::run_command;
 
 fn main() {
     let args = Args::parse();
 
-    let config: Workspace = load_json(&args.config);
-    let package = config
-        .packages
-        .iter()
-        .find(|p| p.name == args.package)
-        .unwrap_or_else(|| {
-            eprintln!("Package not found: {}", args.package);
-            std::process::exit(1);
-        });
-
-    let package_config_path = package.root.join("snoop.json");
-    let package_config: PackageManifest = load_json(&package_config_path);
-    let command = package_config
-        .commands
-        .iter()
-        .find(|c| c.name == args.command)
-        .unwrap_or_else(|| {
-            eprintln!("Command not found: {}", args.command);
-            std::process::exit(1);
-        });
-
-    println!("Running command: {}", command.command);
+    run_command(&args.root_path, args.package.as_deref(), &args.command).unwrap_or_else(|e| {
+        eprintln!("{}", e);
+        exit(1);
+    });
 }
